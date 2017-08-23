f76b0910b85607d337c25badfd7279ffcc6519da
Heroqu <hero.qu@gmail.com>
2017-08-23 19:47:56 +0300

    comment edit

--- test/hashes/erroneous.js
+++ test/hashes/erroneous.js
@@ -1,18 +1,17 @@
 const crypto = require('crypto')
 
 // let's make a createHash implementation that
-// returns a hash obkect that produces an error when
-// update is called:
+// returns a hash object producing an error when
+// update is called
 
 const hash = {
-  update(chunk){
+  update (chunk) {
     throw new Error('hash function update call error')
   },
-  digest(format){
+  digest (format) {
     return 'fake digest, sorry.'
   }
 }
-
 
 function createHash () {
   return hash
